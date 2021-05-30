88ade97462fdc17eb976da211ed621ba2f0f303d
Greck-18 <greck1111@mail.ru>
2021-05-31 00:42:42 +0300

    user with registration and authorization

--- user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-class AdminUser(admin.ModelAdmin):
-    list_display = ['username', 'is_dealer']
-
-
-admin.site.register(User, AdminUser)
